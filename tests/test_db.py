bbe3594f7576b37ebe56d501ec25f85bb8a9c521
Husni Ali Husni <husni1972@yahoo.com.br>
2025-06-15 18:47:43 -0300

    Curso FastAPI - Aula 10 - Exercicios

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,7 +3,7 @@ from dataclasses import asdict
 import pytest
 from sqlalchemy import select
 
-from fastapi_zero.models import User
+from fastapi_zero.models import Todo, User
 
 
 @pytest.mark.asyncio
@@ -27,4 +27,50 @@ async def test_create_user(session, mock_db_time):
         'password': 'secret',
         'created_at': time,
         'updated_at': time,
+        'todos': [],
     }
+
+
+# @pytest.mark.asyncio
+# async def test_create_todo(session, user, mock_db_time):
+#     # with mock_db_time(model=Todo) as time:
+#     todo = Todo(
+#         title='Test Todo',
+#         description='Test Description',
+#         state='draft',
+#         user_id=user.id,
+#     )
+
+#     session.add(todo)
+#     await session.commit()
+
+#     todo = await session.scalar(select(Todo))
+
+#     assert asdict(todo) == {
+#         # 'created_at': time,  # .isoformat,
+#         'description': 'Test Description',
+#         'id': 1,
+#         'state': 'draft',
+#         'title': 'Test Todo',
+#         # 'updated_at': time,  # .isoformat,
+#         'user_id': 1,
+#     }
+
+
+@pytest.mark.asyncio
+async def test_user_todo_relationship(session, user):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Description',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    await session.commit()
+    # Atualiza as informacoes do usuario com a lista de Todo (Ver Models)
+    await session.refresh(user)
+
+    user = await session.scalar(select(User).where(User.id == user.id))
+
+    assert user.todos == [todo]

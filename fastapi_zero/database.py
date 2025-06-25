8483a4ce4ea383d861585f14ad3548d9114d44a6
Husni Ali Husni <husni1972@yahoo.com.br>
2025-06-24 22:33:03 -0300

    Aula 12 - Integracao Continua 01

--- fastapi_zero/database.py
+++ fastapi_zero/database.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from fastapi_zero.settings import Settings
 
-engine = create_async_engine(Settings().DATABASE_URL)
+engine = create_async_engine(
+    Settings().DATABASE_URL,
+    max_overflow=10,
+    pool_size=5
+)
 
 
 # ##-> o "# pragma: no cover" serve para anular a necessidade de teste
